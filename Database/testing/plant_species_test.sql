eead56e74c076c59b96ad8602449a45239a67344
jacksonlmr <38480226+jacksonlmr@users.noreply.github.com>
2025-11-30 11:29:09 -0800

    Removed api and database documentation from readme.

--- Database/testing/plant_species_test.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO plant_species (common_name, scientific_name, img_url)
-    VALUES (
-            'test_name',
-            'test_sci_name',
-            'API_test_img.png'
-            );
