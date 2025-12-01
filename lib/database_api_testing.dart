8fa540acb0b5bd1001ca6411436a83e4c4507b94
Dyllpickle4 <dyllpickle4@gmail.com>
2025-12-01 14:44:49 -0800

    Fixed API testing file for merge

--- lib/database_api_testing.dart
+++ lib/database_api_testing.dart
@@ -2,22 +2,10 @@ import 'package:flutter/material.dart';
 
 import 'database_utils.dart';
 
-//parameters for incorrect_identification
-int identificationId = 1;
-int correctSpeciesId = 2;
-int incorrectSpeciesId = 3;
-// String api_url =
-
 //testing params for user registration
 String username = "testUser-1", email = "testUser-1@unr.edu", passwordHash = "@!)!KAL@!()A:L<DWAEKL", passwordHash2 = "@!)!KAL@!";
 
 Future<void> main(List<String> arguments) async {
-  submitIncorrectIdentification(
-    identificationId: identificationId,
-    correctSpeciesId: correctSpeciesId,
-    incorrectSpeciesId: incorrectSpeciesId,
-  );
-
   int registrationUserID = await submitUserRegistration(
     email: email, 
     username: username, 
