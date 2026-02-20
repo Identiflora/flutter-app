a7a5b9664d1d436c6fc196de4647140c3f4b75bf
nichols-selenium <sailorstraversal@gmail.com>
2026-02-19 16:20:58 -0800

    created friends button

--- lib/friends_utils.dart
+++ lib/friends_utils.dart
@@ -1,4 +1,41 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
-
 import 'package:identiflora/database_utils.dart';
+
+//friends button
+class FriendsHomescreenButton extends StatelessWidget {
+  const FriendsHomescreenButton({super.key});
+
+  @override
+ Widget build(BuildContext context) {
+
+    return SafeArea(
+      child: Align(
+        alignment: Alignment.bottomRight,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+    
+            child: TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const FriendsHomescreenButton()),
+                );
+              }, //end child text button
+              child: const Text ("Friends",
+              style: TextStyle(fontSize: 18),
+              ), //end child text button
+            ),
+        ),
+      ),
+    );
+  } //end build widget
+} // end FriendsHomescreenButton
+
+//screen you navigate to. Returns scaffold ()
+//class FriendsScreen extends StatelessWidget {
+//  const FriendsScreen({super.key});
+  //return 
+//}
+
+//HOMESCREEN BUTTON
