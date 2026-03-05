bd3419990a348cd1666029c95678684d78206f5d
Dyllpickle4 <dyllpickle4@gmail.com>
2026-03-04 18:54:21 -0800

    Cleaned up main by removing stale comment and model loading screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,3 @@
-// Don't know where to put this but during refactoring/optimization we should create some default
-// styles for things like colors, buttons, text formatting etc. I believe it goes in the main app
-// build function in main like how the theme is, but lmk what y'all think
-
-// ^ especially for default padding
-
 import 'package:flutter/material.dart';
 import 'package:identiflora/gallery_utils.dart';
 import 'package:identiflora/leaderboard_utils.dart';
@@ -63,44 +57,6 @@ class HomeScreen extends StatelessWidget {
           GalleryWidget(),
           LeaderboardWidget(),
         ],
-      ),
-    );
-  }
-}
-
-// Temporary loading screen for the model. This should be moved to a new utils file or replaced with the appropriate code when created
-
-// We can probably delete this now --Mark
-class ModelLoadingScreen extends StatelessWidget {
-  const ModelLoadingScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FittedBox(
-              fit: BoxFit.scaleDown,
-              child: Text(
-                "Please wait...\nIdentifying your plant!",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Theme.of(context).colorScheme.primary,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 32.0),
-              child: CircularProgressIndicator(
-                color: Theme.of(context).colorScheme.primary,
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
